9be640a26161e2c2caf0122788530f66825b47d9
Fatima Rahmani <fatima.rahmani79@gmail.com>
2025-12-18 15:24:47 +0430

    complete project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 100;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ee538071-d617-41e1-92d2-5a6efb672f28';
+export const MODAL_CLOSE_SEC = 2.5;
